5777398e705de462a84f0321238f664c853d549f
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-03-03 22:48:48 -0500

    add portal config

--- pkg/inet256ipv6/portal.go
+++ pkg/inet256ipv6/portal.go
@@ -56,25 +56,42 @@ func RunPortal(ctx context.Context, params PortalParams) error {
 	if err := configureInterface(devName, localIPv6); err != nil {
 		return err
 	}
+	p := portal{
+		log:     log,
+		af:      af,
+		network: params.Network,
+		dev:     dev,
+	}
+	return p.run(ctx)
+}
 
+type portal struct {
+	log *logrus.Logger
+	af  AllowFunc
+
+	network inet256.Network
+	dev     tun.Device
+}
+
+func (p *portal) run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		return outboundLoop(ctx, params.Network, dev)
+		return p.outboundLoop(ctx)
 	})
 	eg.Go(func() error {
-		return inboundLoop(ctx, params.Network, dev, af)
+		return p.inboundLoop(ctx)
 	})
 	eg.Go(func() error {
 		for {
 			select {
-			case e := <-dev.Events():
+			case e := <-p.dev.Events():
 				switch e {
 				case tun.EventUp:
-					log.Info("device up")
+					p.log.Info("device up")
 				case tun.EventDown:
-					log.Info("device down")
+					p.log.Info("device down")
 				case tun.EventMTUUpdate:
-					log.Info("mtu update")
+					p.log.Info("mtu update")
 				}
 			case <-ctx.Done():
 				return ctx.Err()
@@ -84,7 +101,8 @@ func RunPortal(ctx context.Context, params PortalParams) error {
 	return eg.Wait()
 }
 
-func outboundLoop(ctx context.Context, network inet256.Network, dev tun.Device) error {
+func (p *portal) outboundLoop(ctx context.Context) error {
+	dev := p.dev
 	mtu, err := dev.MTU()
 	if err != nil {
 		return err
@@ -96,23 +114,22 @@ func outboundLoop(ctx context.Context, network inet256.Network, dev tun.Device) 
 			return err
 		}
 		data := buf[tunOffset:n]
-		if err := handleOutbound(ctx, network, data); err != nil {
-			logrus.Warn("skipping packet: ", err)
+		if err := p.handleOutbound(ctx, data); err != nil {
+			p.log.Warn("skipping packet: ", err)
 		}
 	}
 }
 
-func handleOutbound(ctx context.Context, network inet256.Network, data []byte) error {
+func (p *portal) handleOutbound(ctx context.Context, data []byte) error {
+	network := p.network
 	header, err := ipv6.ParseHeader(data)
 	if err != nil {
 		return err
 	}
-	// log.Println("packet", header.Src, header.Dst)
-	if !Subnet.Contains(header.Dst) {
-		logrus.Debug("dropping packet not in subnet", header.Dst, header.Src)
-		return nil
+	prefix, nbits, err := IPv6ToPrefix(header.Dst)
+	if err != nil {
+		return err
 	}
-	prefix, nbits := IPv6ToPrefix(header.Dst)
 	dst, err := network.FindAddr(ctx, prefix, nbits)
 	if err != nil {
 		return err
@@ -120,13 +137,13 @@ func handleOutbound(ctx context.Context, network inet256.Network, data []byte) e
 	return network.Tell(ctx, dst, data)
 }
 
-func inboundLoop(ctx context.Context, n inet256.Network, dev tun.Device, af AllowFunc) error {
-	n.OnRecv(func(src, dst inet256.Addr, data []byte) {
-		if !af(src) {
+func (p *portal) inboundLoop(ctx context.Context) error {
+	p.network.OnRecv(func(src, dst inet256.Addr, data []byte) {
+		if !p.af(src) {
 			return
 		}
-		if err := handleInbound(dev, src, data); err != nil {
-			logrus.Warn("ignoring INET256 message: ", err)
+		if err := p.handleInbound(src, data); err != nil {
+			p.log.Warn("ignoring INET256 message: ", err)
 		}
 	})
 	select {
@@ -135,7 +152,7 @@ func inboundLoop(ctx context.Context, n inet256.Network, dev tun.Device, af Allo
 	}
 }
 
-func handleInbound(dev tun.Device, src inet256.Addr, data []byte) error {
+func (p *portal) handleInbound(src inet256.Addr, data []byte) error {
 	header, err := ipv6.ParseHeader(data)
 	if err != nil {
 		return err
@@ -144,17 +161,14 @@ func handleInbound(dev tun.Device, src inet256.Addr, data []byte) error {
 	if !header.Src.Equal(srcIP) {
 		return errors.Errorf("dropping inbound message from wrong source")
 	}
-	_, err = dev.Write(data, tunOffset)
+	_, err = p.dev.Write(data, tunOffset)
 	return err
 }
 
 func configureInterface(iface string, ipAddr net.IP) error {
 	switch runtime.GOOS {
 	case "darwin":
-		if err := ifconfigCmd(iface, "inet6", ipAddr.String(), "prefixlen", "7"); err != nil {
-			return err
-		}
-		return nil
+		return ifconfigCmd(iface, "inet6", ipAddr.String(), "prefixlen", "7")
 	default:
 		return errors.Errorf("unsupported OS: %s", runtime.GOOS)
 	}
--- pkg/inet256ipv6/portal_config.go
+++ pkg/inet256ipv6/portal_config.go
@@ -1,4 +1,57 @@
 package inet256ipv6
 
+import (
+	"crypto/ed25519"
+	"encoding/base64"
+
+	"github.com/brendoncarroll/go-p2p"
+	"github.com/inet256/inet256/pkg/inet256"
+	"gopkg.in/yaml.v3"
+)
+
 type PortalConfig struct {
+	PrivateKey string         `yaml:"private_key"`
+	Allowed    []inet256.Addr `yaml:"allowed"`
+}
+
+func (c *PortalConfig) GetPrivateKey() (p2p.PrivateKey, error) {
+	data, err := base64.StdEncoding.DecodeString(c.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	return inet256.ParsePrivateKey(data)
+}
+
+func (c *PortalConfig) GetAllowFunc() AllowFunc {
+	set := make(map[inet256.Addr]struct{}, len(c.Allowed))
+	for _, a := range c.Allowed {
+		set[a] = struct{}{}
+	}
+	return func(x inet256.Addr) bool {
+		_, exists := set[x]
+		return exists
+	}
+}
+
+func DefaultPortalConfig() PortalConfig {
+	_, privKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		panic(err)
+	}
+	pkBytes, err := inet256.MarshalPrivateKey(privKey)
+	if err != nil {
+		panic(err)
+	}
+	pkb64str := base64.StdEncoding.EncodeToString(pkBytes)
+	return PortalConfig{
+		PrivateKey: pkb64str,
+	}
+}
+
+func ParsePortalConfig(data []byte) (*PortalConfig, error) {
+	var config PortalConfig
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
 }
