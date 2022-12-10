63cb157471a93633747beb248df52dd07b8f7d09
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2022-12-10 15:54:29 -0500

    mesh256: remove util.go

--- pkg/mesh256/mesh256.go
+++ pkg/mesh256/mesh256.go
@@ -3,6 +3,7 @@
 package mesh256
 
 import (
+	"github.com/brendoncarroll/go-p2p"
 	"github.com/brendoncarroll/go-p2p/s/multiswarm"
 	"github.com/inet256/inet256/pkg/inet256"
 	"github.com/inet256/inet256/pkg/peers"
@@ -25,4 +26,9 @@ const (
 
 func NewPeerStore() peers.Store[TransportAddr] {
 	return peers.NewStore[TransportAddr]()
+}
+
+func NewSwarm[T p2p.Addr](x p2p.SecureSwarm[T], peers peers.Store[T]) Swarm {
+	sw := newSwarm(x, peers)
+	return swarmFromP2P(sw)
 }
